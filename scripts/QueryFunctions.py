2248c9a7c585dfd3732db8e4cc4735caf907c025
bobby123jack <gphilbri@gmail.com>
2019-09-12 15:39:37 -0400

    Added imports and header objects to QueryFunctions

---
+++ scripts/QueryFunctions.py
@@ -0,0 +1,12 @@
+import sqlite3
+connection = sqlite3.connect('..\data\warm-up-DB-205.db')
+cursor = connection.cursor()
+
+def QueryAdresses(location):
+    return False
+
+def QueryNumCrimes(location):
+    return False
+
+def QueryCommonCrimes(location):
+    return False
