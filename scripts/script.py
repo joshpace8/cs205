28faedb1c3c75710dc2323c179ae5dfd800dbb11
bobby123jack <gphilbri@gmail.com>
2019-09-12 15:23:13 -0400

    Example Script

--- scripts/script.py
+++ scripts/script.py
@@ -1,1 +1,13 @@
-To push
+import sqlite3
+connection = sqlite3.connect('..\data\warm-up-DB-205.db')
+cursor = connection.cursor()
+
+cursor.execute('SELECT * FROM tblTry LIMIT 0,30')
+row = cursor.fetchall()
+
+for i in row:
+    for j in i:
+        print(j)
+
+connection.commit()
+connection.close()
