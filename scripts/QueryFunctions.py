1fc9e54d3021e2995f71744b327a1bdd426ed81a
George Philbrick <gphilbri@gmail.com>
2019-09-15 20:23:27 -0400

    Query Address Function Parameters Updated
    Changed parameters to be key & column rather than just location

--- scripts/QueryFunctions.py
+++ scripts/QueryFunctions.py
@@ -2,19 +2,21 @@ import sqlite3
 connection = sqlite3.connect('..\data\warm-up-DB-205.db')
 cursor = connection.cursor()
 
-def QueryAdresses(location):
+def QueryAdresses(key, column):
+    key = '\'' + key + '\'' # pad key with quotes to ensure dashes, spaces, and underscores are included
     # Get only addresses from tblCrimes table with neighborhood ID
-    cursor.execute('SELECT INCIDENT_ADDRESS, COUNT(INCIDENT_ADDRESS) FROM tblCrimes where NEIGHBORHOOD_ID=? GROUP BY INCIDENT_ADDRESS ORDER BY COUNT(INCIDENT_ADDRESS) DESC', (location, ))
+    cursor.execute('SELECT INCIDENT_ADDRESS, COUNT(INCIDENT_ADDRESS) FROM tblCrimes where {cl}={ky} GROUP BY INCIDENT_'
+                   'ADDRESS ORDER BY COUNT(INCIDENT_ADDRESS) DESC LIMIT 10'.format(cl=column, ky=key))
     row = cursor.fetchall()
     return_string = ""
 
-    for item in row:
-        if item[0] != "":  # Throw away blank addresses
-            return_string += item[0] + " -- "
-            return_string += str(item[1]) + " Occurrence(s)"
-            return_string += "\n"
-
-    if return_string == "":  # Throw warning that searched neighborhood doesn't exist
+    if row:
+        for item in row:
+            if item[0] != "":  # Throw away blank addresses
+                return_string += item[0] + " -- "
+                return_string += str(item[1]) + " Occurrence(s)"
+                return_string += "\n"
+    else:  # Throw warning that searched neighborhood doesn't exist
         return_string += 'That neighborhood does not exist. Try again with a different Neighborhood.'
 
     return return_string
@@ -26,5 +28,5 @@ def QueryCommonCrimes(location):
     return False
 
 
-str = QueryAdresses("jefferson-park")
+str = QueryAdresses('jefferson-park', 'NEIGHBORHOOD_ID')
 print(str)
