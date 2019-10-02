a4cc2658a17b5f7f25597fc5040e5407d44d124f
George Philbrick <gphilbri@gmail.com>
2019-10-02 18:13:27 -0400

    Help Text updated

--- scripts/script.py
+++ scripts/script.py
@@ -14,6 +14,8 @@ def read_until(read_list, end_str):
 
 
 def parse_query(input_string):
+    if not input_string:
+        return 'Please enter something ... type \'help\' if you need help'
     string_list = input_string.split()
     # top return_column in column : key
     start = string_list.pop(0).lower()
@@ -30,7 +32,7 @@ def parse_query(input_string):
         else:
             return q.QueryTop10(key, column, return_column)
     # crimes in column : key
-    elif start == 'crimes':
+    elif start == 'crimecount':
         junk = read_until(string_list, 'in')
         column = read_until(string_list, ":").lower()
         key = read_until(string_list, '')
@@ -57,9 +59,16 @@ def parse_query(input_string):
     elif start == 'exit':
         return 'Exiting...'
     elif start == 'help':
-        return 'HELP STUFF HERE PLEASE WRITE ME'
+        return 'TOP 10 DATA POINTS SYNTAX:\n' \
+               'top return column in column : key \n' \
+               'CRIME COUNT SYNTAX:\n' \
+               'crimecount in column : key\n' \
+               '1 DATAPOINT SYTAX:\n' \
+               'return column at column : key\n\n' \
+               'OTHER COMMANDS : help, exit, columns, refresh'
     else:
-        return 'Data request must start with \'top\', \'crimes\', or a column name'
+        return 'Data request must start with \'top\', \'crimecount\', or a column name\n' \
+               'Type \'help\' for more info'
 
 
 dbCreate.connect_database()
@@ -67,7 +76,7 @@ read = ''
 while read != 'exit':
     print("\nEnter the column, key, and return data you want to retrieve. ")
     print("(example: top crimes in neighborhood : speer)")
-    print("(example: crimes in neighborhood : speer)")
+    print("(example: crimecount in neighborhood : speer)")
     print("(example: crimes in offense code : 35010)")
     read = input('> ')
     print('\n' + parse_query(read))
